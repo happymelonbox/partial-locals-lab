8cd0bf2a37840870761cb6024ecddd41e67fa7f0
Justin Berger Howes <happymelonbox@gmail.com>
2021-11-11 22:01:26 +1100

    complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,11 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    if params[:query]
+      @students = Student.all.filter{|s| s.name == params[:query]}
+    else 
+      @students = Student.all
+    end
   end
 
   def student_params
